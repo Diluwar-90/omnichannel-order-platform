b59684f76ad6cfd0d2c5939aef0ef3738fc3d1b2
diluwar <diluwar.mca@gmail.com>
2026-09-25 23:53:47 +0530

    docs: add product service OpenAPI documentation

---
+++ services/product-service/src/main/java/com/diluwar/product/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.diluwar.product.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI productServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Product Service API")
+                        .version("1.0.0")
+                        .description(
+                                "REST API for product management in the "
+                                        + "Omnichannel Order Platform"
+                        ));
+    }
+}
